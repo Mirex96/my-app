4a2c67421840800715454ddc4c2b7b20ab42d978
Mirex96 <boar_100@mail.ru>
2026-08-31 02:07:11 +0500

    Максимально простой CvoExam

--- src/components/CvoExam/CvoExam.jsx
+++ src/components/CvoExam/CvoExam.jsx
@@ -4,8 +4,7 @@ const CvoExam = () => {
   return (
     <div style={{ padding: '20px', textAlign: 'center' }}>
       <h2 style={{ color: '#1a3c5e' }}>🎓 Экзамен в ЦВО</h2>
-      <p style={{ fontSize: '18px' }}>Компонент работает!</p>
-      <p>Здесь будет билет + комиссия</p>
+      <p style={{ fontSize: '18px' }}>ЦВО РАБОТАЕТ!</p>
     </div>
   );
 };
